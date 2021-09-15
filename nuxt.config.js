783eab6f8bfa033e4f5e31960953dba39a2dfea3
leo6liu <leo.battalora@gmail.com>
2021-09-15 18:41:15 -0400

    site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'spp-website',
+    title: 'Subpar Programming',
     htmlAttrs: {
       lang: 'en'
     },
