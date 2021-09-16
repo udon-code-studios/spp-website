f3d913838c6a8cca8e298a5a75496da5aad8a2b2
leo6liu <leo.battalora@gmail.com>
2021-09-15 23:34:03 -0400

    home content

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-900 text-gray-400">
     <!-- Subpar Programming Banner -->
-    <div class="flex justify-center items-center py-16 gap-6 text-4xl font-extrabold font-mono ">
+    <div class="flex justify-center items-center py-16 gap-6 text-4xl font-extrabold font-mono">
       <img src="/spooky_light_500x500.png" class="h-12 rounded-2xl" />
       subpar programming
     </div>
@@ -10,14 +10,14 @@
     <NavBar class="sticky top-0" />
 
     <!-- Home Page Content -->
-    <div class="max-w-3xl mx-auto py-16 space-y-6 text-gray-400">
+    <div class="max-w-2xl mx-auto py-16 space-y-6 text-gray-400">
       <p class="text-2xl font-semibold">What is Subpar Programming?</p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
+        Subpar Programming is a team of inexperienced engineers and developers producing subpar projects, guides, and
+        blog posts in hopes of one day producing something which could be deemed not-subpar. Learn more about the team
+        on the <a href="/people" class="hover:underline text-blue-400">People</a> page, or check out some of the subpar
+        source code hosted on our
+        <a href="https://github.com/subparprogramming" target="_blank" class="hover:underline text-blue-400">GitHub</a>.
       </p>
 
       <div class="-mx-16 px-16 py-8 mt-8 space-y-6 shadow-md rounded-lg bg-blue-100 text-gray-600">
@@ -69,11 +69,22 @@
       </div>
 
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
+        This body of text down here serves no informational or informative purpose. It simply is to create the illusion
+        that there is a lot to say about the goings-on at Subpar Programming. In a desparate attempt to make having read
+        this paragraph worthwhile, let us share with you a few inspirational quotes:
+      </p>
+      <p class="text-center">
+        "<em class="font-serif"
+          >I grasped the meaning of the greatest secret that human poetry and human thought and belief have to impart:
+          The salvation of man is through love and in love.</em
+        >"<br />&emsp;— Victor Emil Frankl
+      </p>
+      <p class="text-center">
+        There is no 'i' in team "<em class="font-serif">but there is in win.</em>"<br />&emsp;— Michael Jeffrey Jordan
+      </p>
+      <p class="text-center">
+        "<em class="font-serif">The people who are crazy enough to think they can change the world are the ones who do.</em>"<br />&emsp;—
+        Steven Paul Jobs
       </p>
     </div>
 
