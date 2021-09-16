440ddaa3d7e9f51ec8c4e451c423f84a71bd7182
leo6liu <leo.battalora@gmail.com>
2021-09-15 22:52:00 -0400

    fixed error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,22 +1,33 @@
 <template>
   <a href="/">
-    <div class="flex justify-center items-center space-x-9 min-h-screen bg-gray-900">
-      <img src="/spooky_light_500x500.png" class="w-64 rounded-3xl" />
-      <div class="space-y-4 text-gray-400 font-serif font-bold">
-        <p class="text-8xl">Oh No!</p>
-        <p class="text-3xl">
-          The page you were looking for was not found...
-          <br />
-          ... likely due to some <em>subpar programming</em> 😅
-        </p>
-        <p>click anywhere to return to <u>subparprogramming.org</u></p>
+    <div class="flex flex-col justify-center items-center space-y-20 min-h-screen bg-gray-900 text-gray-400">
+      <div class="flex justify-center items-center space-x-10">
+        <img src="/spooky_light_500x500.png" class="w-64 rounded-3xl" />
+        <div class="space-y-4 font-serif font-bold">
+          <p class="text-8xl">Oh No!</p>
+          <p class="text-3xl">
+            An error occured . . .
+            <br />
+            likely due to some <em>subpar programming</em> 😅
+          </p>
+          <p>click anywhere to return to <u>subparprogramming.org</u></p>
+        </div>
+      </div>
+      <div class="flex justify-center space-x-2">
+        <code>error code: {{ error.statusCode }} </code>
+        <code v-if="error.statusCode === 401">(unauthorized)</code>
+        <code v-if="error.statusCode === 404">(page not found)</code>
+        <code v-if="error.statusCode === 500">(internal server error)</code>
+        <code v-if="error.statusCode === 502">(bad gateway)</code>
       </div>
     </div>
   </a>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["error"],
+};
 </script>
 
 <style>
