4d496af2220e68331a28a1fe6ce695cf7d8b30a5
leo6liu <leo.battalora@gmail.com>
2021-09-17 00:25:17 -0400

    display R/L cards

--- pages/people/index.vue
+++ pages/people/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="max-w-3xl mx-auto py-16 space-y-6 text-gray-400">
-    <div class="">
-      <PeopleLeoBattaloraCard />
+    <PeopleLeoBattaloraCardLeft />
+    <div class="-mx-16 px-16 py-8 space-y-6 shadow-md rounded-lg bg-blue-100 text-gray-600">
+      <PeopleLeoBattaloraCardRight />
     </div>
+    <PeopleLeoBattaloraCardLeft />
   </div>
 </template>
 
