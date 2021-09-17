82af465071eeffd19a1435ca7e5444506fac84fd
leo6liu <leo.battalora@gmail.com>
2021-09-16 20:45:26 -0400

    leo profile info

---
+++ components/people/LeoBattaloraCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid grid-cols-5">
+    <div class="col-span-2 p-4 justify-self-center self-center">
+      <img src="/leo_battalora_1000x1000.jpg" class="w-64 rounded-3xl" />
+    </div>
+    <div class="col-span-3 p-4 space-y-2">
+      <div class="text-2xl font-semibold">Leo Battalora</div>
+      <div><strong>Role:</strong> CEO / Founder / Software Engineer</div>
+      <!-- TODO: attach pdf and create directories for people headshots and resumes -->
+      <div><strong>Resume/CV:</strong> <a href="https://google.com"><u>battalora-leo-2021-09-16.pdf</u></a></div>
+      <div class="italic font-serif">"I hope to one day be decent at this whole software development thing."</div>
+      <div class="flex space-x-1 items-center">
+        <a href="https://www.linkedin.com/in/leo6" target="_blank"><SvgLinkedInLogoSquare class="h-8" /></a>
+        <a href="https://github.com/leo6liu" target="_blank"><SvgGitHubLogoSquare class="h-9" /></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
