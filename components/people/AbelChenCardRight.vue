ee697511ff5c58fe3adf47557986a009297f9f96
leo6liu <leo.battalora@gmail.com>
2021-09-17 00:58:56 -0400

    card comments and formatting

--- components/people/AbelChenCardRight.vue
+++ components/people/AbelChenCardRight.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="grid grid-cols-5">
+    <!-- Profile Content -->
     <div class="col-span-3 p-4 space-y-2 text-right">
+      <!-- Name -->
       <div class="text-2xl font-semibold">Abel Chen</div>
+      <!-- Role -->
       <div><strong>Role:</strong> Developer / Writer</div>
-      <!-- TODO: attach pdf and create directories for people headshots and resumes -->
+      <!-- Resume -->
       <div>
         <strong>Resume/CV:</strong> <a href="https://google.com"><u>chen-abel-2021-09-16.pdf</u></a>
       </div>
+      <!-- Quote -->
       <div class="italic font-serif">"I am so f*cking good at taking dumps in people's faces."</div>
+      <!-- Links -->
       <div class="flex pl-1 space-x-2 justify-end items-center">
-        <a href="https://www.linkedin.com/in/leo6" target="_blank"
-          ><SvgLinkedInLogoSquare class="h-8 hover:text-gray-200 duration-200"
-        /></a>
-        <a href="https://github.com/leo6liu" target="_blank"
-          ><SvgGitHubLogoSquare class="h-9 hover:text-gray-200 duration-200"
-        /></a>
-        <a href="mailto:leo.battalora@gmail.com" target="_blank"
-          ><SvgMailIcon class="h-9 hover:text-gray-200 duration-200"
-        /></a>
+        <a href="https://www.linkedin.com/in/abel-chen-75172a1b7" target="_blank">
+          <SvgLinkedInLogoSquare class="h-8 hover:text-gray-900 duration-200" />
+        </a>
+        <a href="https://github.com/Ramenable" target="_blank">
+          <SvgGitHubLogoSquare class="h-9 hover:text-gray-900 duration-200" />
+        </a>
+        <a href="mailto:achen383@gatech.edu" target="_blank">
+          <SvgMailIcon class="h-9 hover:text-gray-900 duration-200" />
+        </a>
       </div>
     </div>
+
+    <!-- Profile Image -->
     <div class="col-span-2 p-4 justify-self-center self-center">
       <img src="/abel_chen_1000x1000.jpg" class="w-64 rounded-3xl" />
     </div>
--- components/people/LeoBattaloraCardLeft.vue
+++ components/people/LeoBattaloraCardLeft.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="grid grid-cols-5">
+    <!-- Profile Image -->
     <div class="col-span-2 p-4 justify-self-center self-center">
       <img src="/leo_battalora_1000x1000.jpg" class="w-64 rounded-3xl" />
     </div>
+
+    <!-- Profile Content -->
     <div class="col-span-3 p-4 space-y-2">
+      <!-- Name -->
       <div class="text-2xl font-semibold">Leo Battalora</div>
+      <!-- Role -->
       <div><strong>Role:</strong> CEO / Founder / Developer</div>
       <!-- TODO: attach pdf and create directories for people headshots and resumes -->
-      <div><strong>Resume/CV:</strong> <a href="https://google.com"><u>battalora-leo-2021-09-16.pdf</u></a></div>
+      <!-- Resume -->
+      <div>
+        <strong>Resume/CV:</strong> <a href="https://google.com"><u>battalora-leo-2021-09-16.pdf</u></a>
+      </div>
+      <!-- Quote -->
       <div class="italic font-serif">"I hope to one day be decent at this whole software development thing."</div>
+      <!-- Links -->
       <div class="flex pl-1 space-x-2 items-center">
-        <a href="https://www.linkedin.com/in/leo6" target="_blank"><SvgLinkedInLogoSquare class="h-8 hover:text-gray-200 duration-200" /></a>
-        <a href="https://github.com/leo6liu" target="_blank"><SvgGitHubLogoSquare class="h-9 hover:text-gray-200 duration-200" /></a>
-        <a href="mailto:leo.battalora@gmail.com" target="_blank"><SvgMailIcon class="h-9 hover:text-gray-200 duration-200" /></a>
+        <a href="https://www.linkedin.com/in/leo6" target="_blank">
+          <SvgLinkedInLogoSquare class="h-8 hover:text-gray-200 duration-200" />
+        </a>
+        <a href="https://github.com/leo6liu" target="_blank">
+          <SvgGitHubLogoSquare class="h-9 hover:text-gray-200 duration-200" />
+        </a>
+        <a href="mailto:leo.battalora@gmail.com" target="_blank">
+          <SvgMailIcon class="h-9 hover:text-gray-200 duration-200" />
+        </a>
       </div>
     </div>
   </div>
