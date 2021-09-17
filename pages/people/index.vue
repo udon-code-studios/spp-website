11d1e3b176ed472a518c86c59f59eafba0cb5f0f
leo6liu <leo.battalora@gmail.com>
2021-09-16 20:46:31 -0400

    started People page using page layout

---
+++ pages/people/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="max-w-3xl mx-auto py-16 space-y-6 text-gray-400">
+    <div class="">
+      <PeopleLeoBattaloraCard />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "page",
+};
+</script>
+
+<style>
+</style>
