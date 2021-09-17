945ac214291bbebfb2690d806549907cf4ff1122
leo6liu <leo.battalora@gmail.com>
2021-09-17 00:35:02 -0400

    abel chen cards

---
+++ components/people/AbelChenCardLeft.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid grid-cols-5">
+    <div class="col-span-2 p-4 justify-self-center self-center">
+      <img src="/abel_boy.jpg" class="w-64 rounded-3xl" />
+    </div>
+    <div class="col-span-3 p-4 space-y-2">
+      <div class="text-2xl font-semibold">Literal Monkey</div>
+      <div><strong>Role:</strong> Professional Pooper</div>
+      <!-- TODO: attach pdf and create directories for people headshots and resumes -->
+      <div><strong>Resume/CV:</strong> <a href="https://google.com"><u>abel-chen-2021-09-16.pdf</u></a></div>
+      <div class="italic font-serif">"YEET. YEET. YEET. YEET. YEET. YEET."</div>
+      <div class="flex pl-1 space-x-2 items-center">
+        <a href="https://www.linkedin.com/in/leo6" target="_blank"><SvgLinkedInLogoSquare class="h-8 hover:text-gray-200 duration-200" /></a>
+        <a href="https://github.com/leo6liu" target="_blank"><SvgGitHubLogoSquare class="h-9 hover:text-gray-200 duration-200" /></a>
+        <a href="mailto:leo.battalora@gmail.com" target="_blank"><SvgMailIcon class="h-9 hover:text-gray-200 duration-200" /></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- pages/people/index.vue
+++ pages/people/index.vue
@@ -2,9 +2,9 @@
   <div class="max-w-3xl mx-auto py-16 space-y-6 text-gray-400">
     <PeopleLeoBattaloraCardLeft />
     <div class="-mx-16 px-16 py-8 space-y-6 shadow-md rounded-lg bg-blue-100 text-gray-600">
-      <PeopleLeoBattaloraCardRight />
+      <PeopleAbelChenCardRight />
     </div>
-    <PeopleLeoBattaloraCardLeft />
+    <PeopleAbelChenCardLeft />
   </div>
 </template>
 
