d2cd5723f0915ee97ad16b65225469822459b18e
leo6liu <leo.battalora@gmail.com>
2021-09-16 20:44:47 -0400

    blank page layout

---
+++ layouts/page.vue
@@ -0,0 +1,11 @@
+<!-- layout for a blank page with a NavBar and Footer-->
+<template>
+  <div>
+    <div class="relative min-h-screen bg-gray-900 text-gray-400">
+      <NavBar />
+      <Nuxt />
+      <div class="p-12"></div>
+      <Footer />
+    </div>
+  </div>
+</template>
