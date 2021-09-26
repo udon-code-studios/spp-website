d6cca57aa70453bcb5d0b8e54641c73e60dce661
leo6liu <leo.battalora@gmail.com>
2021-09-26 00:19:23 -0400

    blog page dev

---
+++ nuxt-app/components/blog/SideBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col w-64 bg-gray-800 text-gray-400">
+    <div class="flex flex-col py-4 items-center">
+      <NuxtLink to="/" class="text-lg font-semibold">
+        <div
+          class="
+            flex
+            items-center
+            px-4
+            py-2
+            rounded-xl
+            space-x-2
+            hover:shadow-lg
+            hover:bg-gray-400
+            hover:text-gray-800
+            duration-200
+          "
+        >
+          <IconsArrowBack class="h-6" />
+          <p>SPP Home</p>
+        </div>
+      </NuxtLink>
+    </div>
+
+    <!-- Footer Links -->
+    <div class="absolute bottom-0 flex self-center py-8 space-x-3 bg-gray-800 text-gray-400">
+      <a href="https://twitter.com/subpar_program" target="_blank">
+        <IconsTwitter class="h-7 hover:text-gray-200 duration-200" />
+      </a>
+      <a href="https://github.com/subparprogramming" target="_blank">
+        <IconsGithub class="h-7 hover:text-gray-200 duration-200" />
+      </a>
+      <a href="mailto:subparprogramming@googlegroups.com" target="_blank">
+        <IconsMail class="h-7 hover:text-gray-200 duration-200" />
+      </a>
+    </div>
+  </div>
+</template>
--- nuxt-app/pages/blog/index.vue
+++ nuxt-app/pages/blog/index.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="max-w-3xl mx-auto py-16 space-y-6 text-gray-400">
-    <p class="text-2xl font-semibold font-serif">This page is under construction.</p>
-    <p class="font-serif">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+  <div class="flex relative min-h-screen bg-gray-900 text-gray-400">
+    <BlogSideBar />
+    <div class="max-w-3xl mx-auto py-16 space-y-6 text-gray-400">
+      <p class="text-2xl font-semibold font-serif">This page is under construction.</p>
+      <p class="font-serif">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  layout: "page",
-};
+export default {};
 </script>
 
 <style>
