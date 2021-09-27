9db3bae4f6e39fbd08fd277a7bc507ccf4c12bdd
leo6liu <leo.battalora@gmail.com>
2021-09-27 15:44:24 -0400

    blog search bar

---
+++ nuxt-app/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <form class="text-gray-500">
+    <div class="flex items-center border-b border-current py-2">
+      <input
+        class="px-2 w-full bg-transparent focus:outline-none placeholder-gray-500 text-gray-200"
+        type="text"
+        placeholder="Search . . ."
+      />
+      <button class="py-1 px-2" type="submit">
+        <IconsSearchOutline class="w-6" />
+      </button>
+    </div>
+  </form>
+</template>
--- nuxt-app/pages/blog/index.vue
+++ nuxt-app/pages/blog/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="flex relative min-h-screen bg-gray-900 text-gray-400">
     <BlogSideBar />
-    <div class="max-w-3xl mx-auto py-16 space-y-6 text-gray-400">
-      <p class="text-2xl font-semibold font-serif">This page is under construction.</p>
+    <div class="w-3/4 pl-16 py-16 space-y-6 text-gray-400">
+      <div class="flex justify-between items-end">
+        <p class="text-4xl font-semibold font-serif">Blog.</p>
+        <SearchBar class="flex-grow max-w-md"/>
+      </div>
       <p class="font-serif">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
