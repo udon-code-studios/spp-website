59ddeeb29bfba5ef9e1d7494dead99b85006f039
leo6liu <leo.battalora@gmail.com>
2022-05-24 14:50:58 -0400

    rename guides to ideas

---
+++ nuxt-app/pages/ideas/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex relative min-h-screen bg-gray-900 text-gray-400">
+    <SideBar></SideBar>
+    <div class="w-3/4 pl-16 py-16 space-y-6 text-gray-400">
+      <div class="flex justify-between items-end">
+        <p class="text-4xl font-semibold font-serif">Ideas.</p>
+        <SearchBar class="flex-grow max-w-md" />
+      </div>
+      <p class="font-serif">This page is under construction.</p>
+      <p class="font-serif">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
