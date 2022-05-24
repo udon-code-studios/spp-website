ac748fa8adcca90da198507b7b969d8b9177b71a
leo6liu <leo.battalora@gmail.com>
2022-05-24 14:50:28 -0400

    underline and changed guides -> ideas

--- nuxt-app/components/NavBar.vue
+++ nuxt-app/components/NavBar.vue
@@ -1,12 +1,33 @@
 <template>
-  <div class="flex justify-center items-center py-4 gap-4 bg-gray-800 text-gray-400 text-xl font-semibold">
-    <NuxtLink to="/" class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200">Home</NuxtLink>
-    <NuxtLink to="/projects" class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200">
+  <div
+    class="flex justify-center items-center py-4 gap-4 bg-gray-800 text-gray-400 text-xl font-semibold"
+  >
+    <NuxtLink
+      to="/"
+      class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200"
+      >Home</NuxtLink
+    >
+    <NuxtLink
+      to="/projects"
+      class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200"
+    >
       Projects
     </NuxtLink>
-    <NuxtLink to="/guides" class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200">Guides</NuxtLink>
-    <NuxtLink to="/blog" class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200">Blog</NuxtLink>
-    <NuxtLink to="/people" class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200">People</NuxtLink>
+    <NuxtLink
+      to="/ideas"
+      class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200"
+      >Ideas</NuxtLink
+    >
+    <NuxtLink
+      to="/blog"
+      class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200"
+      >Blog</NuxtLink
+    >
+    <NuxtLink
+      to="/people"
+      class="p-4 rounded-xl hover:bg-gray-400 hover:text-gray-800 duration-200"
+      >People</NuxtLink
+    >
   </div>
 </template>
 
@@ -17,5 +38,8 @@ export default {};
 <style>
 .nuxt-link-exact-active {
   font-weight: bold;
+  text-decoration-line: underline;
+  text-underline-offset: 4px;
+  text-decoration-thickness: 3px;
 }
 </style>
